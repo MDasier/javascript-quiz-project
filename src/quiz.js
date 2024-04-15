a56676dd96a0a96a6a40a40cd23f101de4560599
Xavi <valcreativo@gmail.com>
2024-04-15 17:01:55 +0200

    Dia 1 Resuelto

--- src/quiz.js
+++ src/quiz.js
@@ -2,6 +2,42 @@ class Quiz {
     // YOUR CODE HERE:
     //
     // 1. constructor (questions, timeLimit, timeRemaining)
+    constructor (questions, timeLimit, timeRemaining){
+        this.questions = questions;
+        this.timeLimit = timeLimit;
+        this.timeRemaining = timeRemaining;
+        this.correctAnswers =0;
+        this.currentQuestionIndex=0;
+        console.log(this.questions)
+        }
+    getQuestion(){
+        return  this.questions[this.currentQuestionIndex];
+       }
+    moveToNextQuestion(){
+        this.currentQuestionIndex++;
+    }
+    shuffleQuestions(){
+        for (let i = this.questions.length - 1; i > 0; i--) {
+            let j = Math.floor(Math.random() * (i + 1));
+            let temp = this.questions[i];
+            this.questions[i] = this.questions[j];
+            this.questions[j] = temp;
+        }
+    }
+    checkAnswer(answer){
+        if (this.answer === this.questions.answer){
+            this.correctAnswers++
+        }
+    }
+    hasEnded(){
+        if (this.currentQuestionIndex >= this.questions.length){
+            return true;
+        }
+        else{
+            return false;
+        }  
+    }
+    
 
     // 2. getQuestion()
     
