aa2b95a8d81a7b1cda06ee2c9845c401f216eed0
Xavi <valcreativo@gmail.com>
2024-04-16 17:46:29 +0200

    dia2 actualizado

--- src/quiz.js
+++ src/quiz.js
@@ -1,65 +1,54 @@
 class Quiz {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (questions, timeLimit, timeRemaining)
-    constructor (questions, timeLimit, timeRemaining){
-        this.questions = questions;
-        this.timeLimit = timeLimit;
-        this.timeRemaining = timeRemaining;
-        this.correctAnswers =0;
-        this.currentQuestionIndex=0;
-        console.log(this.questions)
-        }
-    getQuestion(){
-        return  this.questions[this.currentQuestionIndex];
-       }
-    moveToNextQuestion(){
-        this.currentQuestionIndex++;
+  // YOUR CODE HERE:
+  //
+  //
+  constructor(questions, timeLimit, timeRemaining) {
+    this.questions = questions;
+    this.timeLimit = timeLimit;
+    this.timeRemaining = timeRemaining;
+    this.correctAnswers = 0;
+    this.currentQuestionIndex = 0;
+    console.log(this.questions);
+  }
+  getQuestion() {
+    return this.questions[this.currentQuestionIndex];
+  }
+  moveToNextQuestion() {
+    this.currentQuestionIndex++;
+  }
+  shuffleQuestions() {
+    for (let i = this.questions.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      let temp = this.questions[i];
+      this.questions[i] = this.questions[j];
+      this.questions[j] = temp;
     }
-    shuffleQuestions(){
-        for (let i = this.questions.length - 1; i > 0; i--) {
-            let j = Math.floor(Math.random() * (i + 1));
-            let temp = this.questions[i];
-            this.questions[i] = this.questions[j];
-            this.questions[j] = temp;
-        }
+  }
+  checkAnswer(answer) {
+    if (this.answer === this.questions.answer) {
+      this.correctAnswers++;
     }
-    checkAnswer(answer){
-        if (this.answer === this.questions.answer){
-            this.correctAnswers++
-        }
+  }
+  hasEnded() {
+    if (this.currentQuestionIndex >= this.questions.length) {
+      return true;
+    } else {
+      return false;
     }
-    hasEnded(){
-        if (this.currentQuestionIndex >= this.questions.length){
-            return true;
-        }
-        else{
-            return false;
-        }  
-    }
-    filterQuestionsByDifficulty(difficulty){
-        let preguntasArr=[]
-        if(typeof difficulty==="number" && (difficulty<4&&difficulty>0)){
-           let preguntasArr=this.questions.filter((cadaPregunta)=>{   
-            console.log(cadaPregunta.difficulty+" test: "+difficulty)         
-                if(cadaPregunta.difficulty>difficulty){
-                    return -1
-                }else if(cadaPregunta.difficulty<difficulty){
-                    return 1
-                }else{
-                    return 0
-                }
-            })
-        }            
-       return preguntasArr
-    }
-    // 2. getQuestion()
-    
-    // 3. moveToNextQuestion()
-
-    // 4. shuffleQuestions()
-
-    // 5. checkAnswer(answer)
-
-    // 6. hasEnded()
+  }
+  filterQuestionsByDifficulty(difficulty){
+    let preguntas = this.questions;
+let preguntasFiltradas = preguntas.filter((pregunta) => {
+  return pregunta.difficulty === difficulty;
+})
+return preguntasFiltradas
+  } 
+  averageDifficulty() {
+    let suma = this.questions.reduce((acc, pregunta) => {
+      return acc + pregunta.difficulty;
+    }, 0);
+    console.log(this.questions.length);
+    console.log(suma);
+    return suma / this.questions.length;
+  }
 }
